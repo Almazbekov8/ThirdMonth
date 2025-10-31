5a773a7bdd37517bf4d08a6ad136e48b13e58eda
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-10-31 15:59:06 +0600

    Создал функцию вывода информиции обо мне

--- books/views.py
+++ books/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+def about_me_view(request):
+    if request.method == 'GET':
+        return HttpResponse(f"Меня зовут Азирет, мне 18 лет." 
+                            "Я изучаю программирование и стремлюсь стать отличным разработчиком.")
+        
