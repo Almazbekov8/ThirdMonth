4a719fec609e89aad1b59ea3df213495142f6848
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-10-31 16:26:49 +0600

    Создал ссылку на функцию

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('about-me/', views.about_me_view, name='about_me'),
     path('current-time/', views.current_time_view, name='current_time'),
+    path('random-quote/', views.random_quote_view, name='random_quote'),
 ]
