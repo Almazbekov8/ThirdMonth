754ab31f2eae5169dcf7ef342bf5da21bfc8beb8
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-10-31 16:16:12 +0600

    Создал функцию вывода текущего времени

--- books/views.py
+++ books/views.py
@@ -6,3 +6,19 @@ def about_me_view(request):
         return HttpResponse(f"Меня зовут Азирет, мне 18 лет." 
                             "Я изучаю программирование и стремлюсь стать отличным разработчиком.")
         
+def current_time(request):
+    if request.method == 'GET':
+        time_str = "12:00"
+        hours, minutes = map(int, time_str.split(":"))
+        if hours < 12:
+            message = "Сейчас утро"
+        elif 12 <= hours < 15:
+            message = "Сейчас обед"
+        elif 15 <= hours < 21:
+            message = "Сейчас вечер"
+        else:
+            message = "Сейчас ночь"
+
+        return HttpResponse(f"Время: {time_str}. {message}")
+    
+    
