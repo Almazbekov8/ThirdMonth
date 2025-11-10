67f780e747f5265404678631c965629dc7a5d927
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-11-10 23:47:00 +0600

    Создал url

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('about-me/', views.about_me_view, name='about_me'),
     path('current-time/', views.current_time_view, name='current_time'),
     path('random-quote/', views.random_quote_view, name='random_quote'),
+    path('book_list/', views.book_list_view, name = 'book_list'),
+    path('book_list/<int:id>', views.book_detail_view, name = 'book_detail'),
 ]
