9f99c634f9dce8a0ee3e205b94857f4b6d9ab67b
Aziret Almazbekov <izmeniziznzagod@gmail.com>
2025-11-10 22:09:41 +0600

    Создал модель book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
+    pages = models.IntegerField()   
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
